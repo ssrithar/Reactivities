aad0868053dc71afc7dac9e7e7bd127d3f66e21e
Saravanan Srithar <ssrithar@microsoft.com>
2020-07-31 01:18:18 -0700

    Using interface in React Component

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './styles.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+import { IActivity } from '../../app/models/activity';
+
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  readonly state: IState = {
+    activities: []
+  }
+
+  componentDidMount() {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+          this.setState({
+            activities: response.data
+          })
+      });
+  }
+
+  render() {
+    return (
+          <div>
+            <Header as='h2'>
+              <Icon name='users' />
+              <Header.Content>Reactivities</Header.Content>
+            </Header>
+            <List>
+            {this.state.activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            ))}
+            </List>                      
+          </div>    
+        );
+  }
+}
+
+export default App;
